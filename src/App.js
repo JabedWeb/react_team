256c4c793984e7651d296fa1ab624e9ed22279f0
jabed366 <jabedhasan231@gmail.com>
2022-04-07 02:06:21 +0600

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import Team from "./Team/Team";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <>
+    <Team title="Shadhin Web Developers" content="Create Website grow your Business" layout="style2" bg="blue"/>
+    <Team title="Our Developers" content="Our developers are top rated " layout="" bg="black"/>
+
+   
+  
+  </>
+  
   );
 }
 
